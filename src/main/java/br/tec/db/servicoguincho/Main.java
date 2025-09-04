8911a0385b35ab4fc0109afea5cdba041ed6bfbe
Marcos Camargo <marcos.camarg0@outlook.com>
2025-09-04 10:55:57 -0300

    Update Main.java

--- src/main/java/br/tec/db/servicoguincho/Main.java
+++ src/main/java/br/tec/db/servicoguincho/Main.java
@@ -12,10 +12,10 @@ import br.tec.db.servicoguincho.simulador.impl.VeiculoImpl;
 public class Main {
 
     public static void main(String[] args) {
-        // Cenário 1 - carro novo - do centro para o bairro A
+
         SimuladorDeOrcamento simulador = new SimuladorDeOrcamentoImpl();
         Veiculo carroNovo = new VeiculoImpl(TipoVeiculo.CARRO, EstadoConservacao.NOVO);
-        Trajeto centroParaBairroA = new TrajetoImpl(10); // Distância C -> A é 10 km
+        Trajeto centroParaBairroA = new TrajetoImpl(10);
         double custoCenario1 = simulador.calcularCustoTotal(carroNovo, centroParaBairroA);
         System.out.println("Cenário 1 - Custo Total do Serviço: R$" + custoCenario1);
     }
